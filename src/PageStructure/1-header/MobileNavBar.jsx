cae125af9213c5ed5065b6052da113e7efefbf88
ingridgsi <isg.ingridguimaraes@gmail.com>
2024-02-06 14:16:11 +0100

    update responsive design

--- src/PageStructure/1-header/MobileNavBar.jsx
+++ src/PageStructure/1-header/MobileNavBar.jsx
@@ -1,8 +1,21 @@
 import MenuList from "./MenuList";
 import Logo from "./Logo";
 import StickNavBar from "./StickNavBar";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function MobileNavBar({ pages, onClick, onClose, isOpen }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    setIsAnimating(true);
+    const timeout = setTimeout(() => {
+      setIsAnimating(false);
+    }, 300); // Adjust the duration of the transition as needed
+
+    return () => clearTimeout(timeout);
+  }, [isOpen]);
+
   return (
     <>
       <StickNavBar type={"mobile"}>
@@ -20,7 +33,7 @@ function MobileNavBar({ pages, onClick, onClose, isOpen }) {
       </StickNavBar>
 
       {isOpen && (
-        <nav className="fixed inset-0 z-10 bg-[#fff] shadow-xl p-4 rounded-lg flex flex-col justify-center items-center">
+        <nav className="fixed inset-0 z-10 opacity-95 bg-[#fff] shadow-xl p-4 rounded-lg flex flex-col justify-center items-center">
           <Logo position="absolute top-3 left-3" />
           <button
             onClick={onClose}
