64f9258b97f50da52e69bb8544d71033ae6a7ea1
ingridgsi <isg.ingridguimaraes@gmail.com>
2024-02-06 13:51:58 +0100

    update responsive design

--- src/Cta.jsx
+++ src/Cta.jsx
@@ -10,7 +10,7 @@ function Cta({ ctaText, to, onClick }) {
       smooth={true}
       offset={-100}
       duration={1000}
-      className="font-bold py-[0.8rem] px-[2.5rem] bg-primary rounded-lg text-[#fff] shadow-3xl hover:text-[#fff] hover:bg-secondary cursor-pointer transition-all duration-500 ease-in-out  "
+      className="font-semibold py-[0.8rem] px-[2.5rem] bg-primary rounded-lg text-[#fff] shadow-3xl hover:text-[#fff] hover:bg-secondary cursor-pointer transition-all duration-500 ease-in-out text-xl md:text-[1.2rem] "
     >
       {ctaText}
     </Link>
